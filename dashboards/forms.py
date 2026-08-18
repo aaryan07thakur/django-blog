994ac8adf43bc8ec087f61bf44a0bb2d23cd5687
Dipesh Thakur <princeaaryan06@gmail.com>
2026-08-18 22:52:32 +0545

    completed edit_user funcationality creating edituserform model in forms.py to remove password access for manager while edit

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -22,6 +22,11 @@ class AddUserForm(UserCreationForm):
 
 
 
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email' ,'first_name', 'last_name', 'is_active' , 'is_staff', 'is_superuser', 'groups', 'user_permissions',)
+
 
 
 
